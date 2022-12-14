aca0c97564a25c3acf6350c8134008afd0c935d8
Matheus Jacobsen <matheusjacobsen@usp.br>
2022-12-14 11:25:15 -0300

    Arrays das Unidades

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
 from os.path import abspath, dirname, join
+
+class TheApp:
+    '''The Application Class.'''
+
+    def __init__(self):
+        # Build GUI
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('combo_units.glade')
+
+         # Get objects
+        self.window = self.builder.get_object('window')
+        self.window.set_title('Conversor de Unidades - Massa e Volume')
+
+        # Array de 4 colunas com a primeira sendo ID e as outras as unidades e quantidades
+        self.quantities_list = Gtk.ListStore(int, str)
+        self.mass_units_list = Gtk.ListStore(int, str)
+        self.volume_units_list = Gtk.ListStore(int, str)
+
